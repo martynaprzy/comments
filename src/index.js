be22d86d9627bd9146ac02e973118c6009232038
martynaprzy <martynajprzybysz@gmail.com>
2019-03-12 15:01:13 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a React component
+const App = () => {
+    return <div>Hi there</div>;
+};
+
+// take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
